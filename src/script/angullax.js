7e16ea5ad2532319184d1172a33a83939c2f198d
Rohit Raheja <rohitraheja88@gmail.com>
2015-05-10 17:34:57 +0530

    Adding demo app files

---
+++ src/script/angullax.js
@@ -0,0 +1,67 @@
+/**
+ * An angularjs directive to create parallax scroll
+ * @version v0.0.1 
+ * @author Rohit Raheja <rohitraheja88@gmail.com>
+ * @license MIT License 
+ */
+'use strict';
+
+angular.module( 'angullax', [] );
+'use strict';
+
+angular.module( 'angullax' )
+	.directive( 'angullax', [ '$interpolate', '$timeout',
+	 function ( $interpolate, $timeout ) {
+
+		var postLinkFuc;
+
+		postLinkFuc = function ( scope, elem, attrs, ctrl, transcludeFn ) {
+
+			transcludeFn( function ( clone ) {
+				var navItems = $( clone ).filter( 'section' ),
+					pageId = 0;
+				scope.navItems = [];
+
+				angular.forEach( navItems, function ( item ) {
+					var navItem = {};
+					navItem.heading = $interpolate( angular.element( item ).attr( 'heading' ) )();
+					navItem.id = 'page' + ( pageId++ );
+					scope.navItems.push( navItem );
+				} );
+
+				$timeout( function () {
+					var pages = $( elem ).find( '[data-role="page"]' );
+
+					angular.forEach( navItems, function ( item, i ) {
+						var el = angular.element( item );
+						$( pages[ i ] ).css( 'background-color', ( '#' + el.attr( 'color' ) ) )
+							.append( el.contents() );
+					} );
+
+					$( pages[ 0 ] ).css( 'padding-top', 60 );
+				}, 0 );
+
+			} );
+
+			scope.selectedTab = 0;
+
+			scope.goToPage = function ( index ) {
+				$( 'html, body' ).animate( {
+		          scrollTop: $( '#page' + index ).offset().top - 60
+		        }, 1000 );
+
+		        scope.selectedTab = index;
+			};
+
+		};
+
+		return {
+			templateUrl: 'views/angullax.html',
+			restrict: 'E',
+			replace: true,
+			transclude: true,
+			link: postLinkFuc,
+			scope: {}
+		};
+
+	} ] );
